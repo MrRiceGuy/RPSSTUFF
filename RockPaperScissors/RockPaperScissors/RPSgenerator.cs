b7404a63ce095cf16dbe4902302e923aca59d590
MrRiceGuy <45943208+MrRiceGuy@users.noreply.github.com>
2022-01-20 20:50:03 -0500

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/RPSgenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class RPSgenerator
+    {
+        static Random rand = new Random();
+        int RPS = rand.Next(4);
+    }
+}
