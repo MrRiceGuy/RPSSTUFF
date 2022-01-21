d71bae897f62d18843bbd9db0ccfbb0105205507
MrRiceGuy <45943208+MrRiceGuy@users.noreply.github.com>
2022-01-20 20:48:10 -0500

    Add files via upload

---
+++ RPS.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information);
+
+
+while (true)
+{
+    Console.WriteLine("Please enter a name.");
+
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Please select an oponent.");
+
+    string oponnent = Console.ReadLine();
+
+    Console.WriteLine("Please select rock, paper, or scissors.");
+}
+
+abstract class Player (string name){
+
+
+}
+public enum RPS
+{
+    Rock,
+    Paper,
+    Scissors
+}
